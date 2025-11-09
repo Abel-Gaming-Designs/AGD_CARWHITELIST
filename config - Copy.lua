a76c265d0b64a2f67b3be864d338f747cdbb9482
Tyler Abel <lcpdpolice4939@gmail.com>
2025-11-08 22:28:43 -0500

    Add files via upload

---
+++ config - Copy.lua
@@ -0,0 +1,65 @@
+Config = {}
+
+Config.BlacklistPassword = 'PASSWORD' -- This can be used to grant temporary access to blacklisted vehicles
+
+Config.BlacklistVehicles = {
+    'sasphellcat',
+	'demonboi',
+    'saspcessna',
+    'saspheli',
+    'saspboat',
+    'saspcoroner',
+    'saspbus',
+    'minirango',
+    'durango3',
+    'leoraptor',
+    '17fusionrb',
+    'sasptahoe21',
+
+    -- CIV CARS --
+    'rcbandito',
+    'marshall',
+    'monster',
+    'zr3803',
+    'zr3802',
+    'oppressor2',
+    'monster5',
+    'cerberus3',
+    'deathbike3',
+    'monster3',
+    'monster4',
+    'slamvan6',
+    'minitank',
+    'formula2',
+    'formula',
+    'openwheel1',
+    'openwheel2',
+    'caracara',
+    'khanjali',
+    'tampa3',
+    'limo2',
+    'boxville5',
+    'dune5',
+    'dune4',
+    'phantom2',
+    'oppressor',
+    'deathbike2',
+    'jb700',
+    'jb700w',
+    'ruiner2',
+    'deluxo',
+    'voltic2'
+}
+
+Config.ApprovedLicenses = { --(Please note this uses license only. Steam, Discord, and others are not supported)
+    'license:xxxxxxxxxxxxxxxxxxxxx',
+    'license:xxxxxxxxxxxxxxxxxxxxxxx'
+}
+
+Config.DirectorVehicles = {
+	'22jeep'
+}
+
+Config.DirectorLiceneses = {
+    'license:xxxxxxxxxxxxxxxxxxxxxxxxxxx'
+}
